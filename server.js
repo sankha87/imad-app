8d2cde4da436eda32d82803b0625944c0da70841
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2017-09-09 10:28:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,14 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/article-one',function(req,res) {
+  res.send("article one is requested and will be served here");
+});
+
+
+
+
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
