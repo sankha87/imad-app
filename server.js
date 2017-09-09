a0ccc64ae6ed66b479af4837d8f11852ae953119
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2017-09-09 10:30:24 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ app.get('/article-one',function(req,res) {
 });
 
 app.get('/article-two',function(req,res) {
-  res.send("article one requested and will be served here");
+  res.send("article two requested and will be served here");
 });
 
 app.get('/article-three',function(req,res) {
