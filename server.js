2fd6af5d8f23c560be972a029da94c7393254cdf
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2017-09-10 20:02:35 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ var articles = {
     title : 'Article one | sankhadeep',
     heading : 'Article one',
     date : ' 9 Sept 2017 ',
-    content: `<p>
+content: `      <div>
+<a href="/">article-one.html</a>
+</div>
+            <p>
                 This is the content for my first article. This is the content for my first article. This is the content for my first article
               </p>    
               <p> 
