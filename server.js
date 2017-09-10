064c96945a9717fe77543eed528c1304a61d0258
Sankhadeep Pujaru <sankhadeep.pujaru2008@gmail.com>
2017-09-10 14:28:44 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -81,13 +81,13 @@ app.get('/:articleName',function(req,res) {
    res.send(createTemplate(articles[articleName]));
 });
 
-app.get('/article-two',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
-});
+// app.get('/article-two',function(req,res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'article-two.html'));
+//});
 
-app.get('/article-three',function(req,res) {
-  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
-});
+//app.get('/article-three',function(req,res) {
+//  res.sendFile(path.join(__dirname, 'ui', 'article-three.html'));
+//});
 
 app.get('/article-four',function(req,res) {
   res.send("article four requested and will be served here");
